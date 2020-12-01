2747af21b4a3cc8ebc3512b7baf9b90229588431
AHG <ahakangenc92@gmail.com>
2020-12-01 18:44:46 +0300

    Add functionality to create new playlists and add songs to playlists

--- 25.3_Assessment_4_Database_DJ_Project/models.py
+++ 25.3_Assessment_4_Database_DJ_Project/models.py
@@ -8,22 +8,37 @@ db = SQLAlchemy()
 class Playlist(db.Model):
     """Playlist."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = "playlists"
+
+    # set table columns
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
 
 
 class Song(db.Model):
     """Song."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = "songs"
 
+    # set table columns
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    artist = db.Column(db.Text, nullable=False)
+
+    playlists = db.relationship("Playlist", secondary="playlists_songs", backref="songs")
 
 class PlaylistSong(db.Model):
     """Mapping of a playlist to a song."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = "playlists_songs"
+
+    # set table columns
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlists.id'), nullable=False)
+    song_id = db.Column(db.Integer, db.ForeignKey('songs.id'), nullable=False)
 
 
-# DO NOT MODIFY THIS FUNCTION
 def connect_db(app):
     """Connect to database."""
 
