06ca0894e9485ea22d767c60fe1cc52dd03a46d5
AHG <ahakangenc92@gmail.com>
2020-12-03 17:10:51 +0300

    Add profile update form and updating user info functionality

--- 26_Twitter_Clone/app.py
+++ 26_Twitter_Clone/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 import pdb
 
@@ -216,7 +216,35 @@ def stop_following(follow_id):
 def profile():
     """Update profile for current user."""
 
-    # IMPLEMENT THIS
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    form = UserEditForm(obj=g.user)
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data, form.password.data)
+
+        if user:
+            user.username = form.username.data
+            user.email = form.email.data
+            user.image_url = form.image_url.data or None
+            user.header_image_url = form.header_image_url.data or None
+            user.bio = form.bio.data 
+            user.location = form.location.data
+
+            db.session.commit()
+
+            flash("Update Successful.", "success")
+            return redirect(f'/users/{user.id}')
+
+        else:
+            # This is the correct code based on the project instructions but I think redirecting users to the homepage here is not a good user experience. Just reloading the form with the flash message and asking them to type their password again would be a better user experience in my opinion.
+            flash("Incorrect password.", "danger")
+            return redirect('/')
+
+    return render_template('/users/edit.html', form=form)
+    
 
 
 @app.route('/users/delete', methods=["POST"])
