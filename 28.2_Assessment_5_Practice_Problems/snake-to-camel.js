21856db75ce6ef4f1ecae9db0c304188a92031f7
AHG <ahakangenc92@gmail.com>
2020-12-09 13:52:58 +0300

    Answer practice problems #1

--- 28.2_Assessment_5_Practice_Problems/snake-to-camel.js
+++ 28.2_Assessment_5_Practice_Problems/snake-to-camel.js
@@ -1,2 +1,11 @@
-function snakeToCamel() {}
+function snakeToCamel(str) {
+  let res =  str.split('_').map(function(word, index){
+    if (index === 0) {
+      return word;
+    } else {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+  }).join('');
 
+  return res;
+}
