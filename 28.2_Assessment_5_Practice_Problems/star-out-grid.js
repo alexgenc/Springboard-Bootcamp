790c4853de439b7638ec96a832c333931a1efd21
AHG <ahakangenc92@gmail.com>
2020-12-09 15:13:18 +0300

    Answer practice problem #2

--- 28.2_Assessment_5_Practice_Problems/star-out-grid.js
+++ 28.2_Assessment_5_Practice_Problems/star-out-grid.js
@@ -1,2 +1,32 @@
 function starOutGrid(grid) {
+  const arrIdx = [];
+  const starIdx = [];
+  // find the arrays that contain a '*'
+  grid.forEach((arr, index) => arr.includes('*') ? arrIdx.push(index) : null);
+  
+  // find the index of star within those arrays
+  for (i of arrIdx) {
+    starIdx.push(grid[i].findIndex(el => el === '*'));
+  }
+
+  // star out all the items in the arrays that contain a star
+  for (i of arrIdx) {
+    // get the nested array that contains a star
+    containingArr = grid[i];
+    // star out each item in those arrays
+    containingArr.forEach((item, i) => item !== '*' ? containingArr[i] = '*' : null);
+  }
+
+  // star out the matching columns in the remaining arrays
+  for (item of arrIdx) {
+    for (index of starIdx) {
+      for (i = 0; i < grid.length; i++) {
+        if (i !== item) {
+          grid[i][index] = '*';          
+        }
+      }
+    }
+  }
+
+  return grid;
 }
