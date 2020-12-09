ea56333724457dafa0a566e7e418562a5f868380
AHG <ahakangenc92@gmail.com>
2020-12-09 23:36:17 +0300

    Build backend API and add a simple frontend

--- 28.3_Assessment_5_Lucky_Number_Project/app.py
+++ 28.3_Assessment_5_Lucky_Number_Project/app.py
@@ -1,10 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import requests
+import random
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def homepage():
     """Show homepage."""
 
     return render_template("index.html")
+
+
+@app.route("/api/get-lucky-num", methods=["POST"])
+def get_lucky_num():
+    """Return lucky nubmer and year trivia"""
+    try:
+        name = request.json['name']
+        email = request.json['email']
+        year = request.json['year']
+        color = request.json['color']
+
+        lucky_num = random.randint(1, 100)
+        num_fact = requests.get(f"http://numbersapi.com/{lucky_num}").text
+        year_fact = requests.get(f"http://numbersapi.com/{year}/year").text
+
+        response = (
+            {
+                "num": {
+                    "fact": str(num_fact),
+                    "num": str(lucky_num),
+                },
+                "year": {
+                    "fact": str(year_fact),
+                    "year": str(year)
+                }
+            })
+
+        return jsonify(response)
+
+    except:
+
+        errors = {}
+    
+        content = request.json
+        required = ['name', 'email', 'year', 'color']
+
+        for item in required:
+            if item not in content:
+                errors[item] = "This field is required"
+
+        if content['color'] in content:      
+            if content['color'] != "red" or "green" or "orange" or "blue":
+                errors['color'] = "Invalid value, must be one of: red, green, orange, blue."
+        
+        res = (
+            {
+                "errors": errors
+            }
+        )
+
+        return (jsonify(res), 400)
+    
+        
+        
--- 28.3_Assessment_5_Lucky_Number_Project/static/lucky.js
+++ 28.3_Assessment_5_Lucky_Number_Project/static/lucky.js
@@ -1,12 +1,50 @@
+namee = document.getElementById('name');
+email = document.getElementById('email');
+year = document.getElementById('year');
+color = document.getElementById('color');
+results = document.getElementById('lucky-results');
+
 /** processForm: get data from form and make AJAX call to our API. */
 
-function processForm(evt) {
-}
+async function processForm(evt) {
+  evt.preventDefault();
 
+  if (namee.value !== '' && email.value !== '' && year.value !== '' && color.value !== '') {
+    config = {
+      "name": `${name.value}`,
+      "email": `${email.value}`,
+      "year": `${year.value}`,
+      "color": `${color.value}`
+    };
+    
+    res = await axios.post('/api/get-lucky-num', config);
+
+    handleResponse(res);
+
+    namee.value = '';
+    year.value = '';
+    email.value = '';
+    color.value = '';
+
+  } else {
+    alert("Missing Form Fields");
+  }
+}
 /** handleResponse: deal with response from our lucky-num API. */
 
-function handleResponse(resp) {
+async function handleResponse(resp) {
+  let data = resp.data
+  if (data.num) {
+    results.innerText = `
+    Your favorite num is ${data.num.num} ==> ${data.num.fact}
+    Your birth year is ${data.year.year} ==> ${data.year.fact}`
+  }
+  if (data.errors) {
+    
+  }
+  
 }
 
 
 $("#lucky-form").on("submit", processForm);
+
