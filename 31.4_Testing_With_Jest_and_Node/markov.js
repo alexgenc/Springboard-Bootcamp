a64bd7cafc321030b7b6e9ecaae282be30719ab2
AHG <ahakangenc92@gmail.com>
2020-12-21 17:41:42 +0300

    Build a simple markov machine text generator

--- 31.4_Testing_With_Jest_and_Node/markov.js
+++ 31.4_Testing_With_Jest_and_Node/markov.js
@@ -1,6 +1,5 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
 
   /** build markov machine; read in text.*/
@@ -17,13 +16,49 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = new Map();
+
+    for (let i = 0; i < this.words.length; i += 1) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      if (chains.has(word)) {
+        chains.get(word).push(nextWord);
+      } else {
+        chains.set(word, [nextWord]);
+      } 
+    }
+
+    this.chains = chains;
   }
 
+  static getRandomWord(map) {
+    let keys = Array.from(map.keys());
+
+    let randomIndex = Math.floor(Math.random() * keys.length);
+
+    let randomKey = keys[randomIndex];
+  
+    let randomWord = map.get(randomKey);
+
+    return randomWord
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let randomWord = MarkovMachine.getRandomWord(this.chains);
+    let output = [];
+    
+    while (output.length < numWords && randomWord !== null) {
+      output.push(randomWord)
+      randomWord = MarkovMachine.getRandomWord(this.chains);
+    }
+
+    return output.join(" ");
   }
 }
+
+module.exports = {
+  MarkovMachine
+};
