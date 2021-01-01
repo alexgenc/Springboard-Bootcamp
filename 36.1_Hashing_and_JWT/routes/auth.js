6bf7a2eade3619f3fa07f30f7469eba2f6787023
AHG <ahakangenc92@gmail.com>
2021-01-01 20:53:48 +0300

    Build authentication routes

--- 36.1_Hashing_and_JWT/routes/auth.js
+++ 36.1_Hashing_and_JWT/routes/auth.js
@@ -1,9 +1,31 @@
+const express = require('express');
+const router = new express.Router();
+const ExpressError = require('../expressError');
+const User = require('../models/user');
+
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
 
+router.post("/login", async (req, res, next) => {
+  try {
+    // Destructure needed fields from req.body
+    const { username, password } = req.body;
+    // Log in user, this also updates user's last_login_at and returns user's JWT
+    const token = await User.authenticate(username, password);
+    // Check if login is successful, if so, return token
+    if (token) {
+      return res.json({token});
+    }
+    // Else, throw error
+    throw new ExpressError("Invalid username/password!", 400);
+  } catch(e) {
+    return next(e);
+  }
+})
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +33,22 @@
  *
  *  Make sure to update their last-login!
  */
+
+ router.post("/register", async (req, res, next) => {
+   try {
+      // Destructure needed fields from req.body
+      const { username, password, first_name, last_name, phone } = req.body;
+      // Register user
+      const result = await User.register({username, password, first_name, last_name, phone});
+      // Log in user, this also updates user's last_login_at and returns user's JWT
+      const token = await User.authenticate(username, password);
+
+      return res.json({token});
+
+   } catch(e) {
+     return next(new ExpressError("Unable to register. Make sure all requierd fields are submitted.", 400));
+   }
+})
+
+
+ module.exports = router;
