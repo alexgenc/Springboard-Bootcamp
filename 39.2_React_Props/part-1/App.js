2ac061ba79e61cd6cb2bb1cf5bb028af7958d208
AHG <ahakangenc92@gmail.com>
2021-01-06 14:04:18 +0300

    Commit part 1 of React props exercise

---
+++ 39.2_React_Props/part-1/App.js
@@ -0,0 +1,9 @@
+
+const App = () => (
+  <div>
+    <FirstComponent />
+    <NamedComponent name="Alex" />
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById("root")); 
---
+++ 39.2_React_Props/part-1/FirstComponent.js
@@ -0,0 +1,5 @@
+const FirstComponent = () => (
+    <div>
+    <h1>My Very First Component!</h1>
+    </div>
+)
