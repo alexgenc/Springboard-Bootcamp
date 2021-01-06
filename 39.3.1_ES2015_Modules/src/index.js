e8508dbaa8ddccde0cffd3ff21341d86002ad9b4
AHG <ahakangenc92@gmail.com>
2021-01-06 20:16:50 +0300

    App file

---
+++ 39.3.1_ES2015_Modules/src/index.js
@@ -0,0 +1,12 @@
+import foods from './foods';
+import {choice, remove} from './helpers';
+
+let randomFruit = choice(foods)
+
+console.log(`I'd like one ${randomFruit} please!`);
+console.log(`Here you go: ${randomFruit}`);
+console.log(`Delicious! May I have another?`);
+
+let remainingFood = remove(foods, randomFruit);
+
+console.log(`I'm sorry, we're all out. We have ${remainingFood.length} left`);
