61f48a0690669f138c28da325a341a9bc249cb8c
AHG <ahakangenc92@gmail.com>
2021-01-21 14:44:20 +0300

    Add filtering functionality to findAll method

--- 37_Cumulative_Project2_Jobly/models/company.js
+++ 37_Cumulative_Project2_Jobly/models/company.js
@@ -49,15 +49,63 @@ class Company {
    * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
    * */
 
-  static async findAll() {
-    const companiesRes = await db.query(
-          `SELECT handle,
-                  name,
-                  description,
-                  num_employees AS "numEmployees",
-                  logo_url AS "logoUrl"
-           FROM companies
-           ORDER BY name`);
+  static async findAll(query = {}) {
+
+    
+    const { name, minEmployees, maxEmployees } = query; 
+
+    // Check min. number of employees isn't greater than max. number of employees.
+    if (minEmployees > maxEmployees) {
+      throw new BadRequestError("Minimum number of employees can't be greater than maximum number of employees!")
+    } 
+
+    // Create an empty array to store serialized parameters; ex: x = $1, y = $2.
+    const serializedFilters = [];
+    // Create an empty array to store query values to filter companies.
+    const filterValues = [];
+
+
+    // We only have 3 query parameters to filter by. Name, minEmployees, and maxEmployees. Check if each one exists and if so, store needed values in the 2 arrays created above.
+
+    if (name)  { 
+      serializedFilters.push(`name ILIKE $${serializedFilters.length + 1}`)
+      filterValues.push(`%${name}%`) 
+    } 
+    
+    if (minEmployees) {
+      serializedFilters.push(`num_employees >= $${serializedFilters.length + 1}`)
+      filterValues.push(minEmployees);
+    }
+    
+    if (maxEmployees) {
+      serializedFilters.push(`num_employees <= $${serializedFilters.length + 1}`)
+      filterValues.push(maxEmployees)
+    } 
+
+    // Initialize a filter variable. This will be a string containing our serialized parameters.
+    let filter;
+    
+    // If any query parameters are passed in, i.e., length > 0,  set filter variable accordingly. 
+    serializedFilters.length > 0 ? filter = `WHERE ${serializedFilters.join(" AND ")}` : null
+    
+    // Create an initial SQL query
+    let dbQuery = `SELECT handle,
+                    name,
+                    description,
+                    num_employees AS "numEmployees",
+                    logo_url AS "logoUrl"
+                    FROM companies
+                  `;
+
+    // If filter exists, meaning there are query parameters passed in, add filter to our SQL query. Here, filter is the WHERE statement of our SQL query. If filter is falsy, that means there are no query parameters, so our SQL query won't have a WHERE statement in it.
+    filter ? dbQuery = dbQuery + filter : null
+
+    // Lastly, add an ORDER BY statement to our SQL query.
+    dbQuery = dbQuery + ` ORDER BY name`
+
+    // Make a database query and return the results. If there are no filters, results will list all existing companies.
+    const companiesRes = await db.query(dbQuery, filterValues);
+
     return companiesRes.rows;
   }
 
