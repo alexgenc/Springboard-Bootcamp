745c87b76393383d190a70b56f73364c1a722a3a
AHG <ahakangenc92@gmail.com>
2021-01-28 14:09:00 +0300

    Initial commit

---
+++ 40.3_Assessment_8_React_Madlibs/src/App.js
@@ -0,0 +1,12 @@
+import Madlib from './components/Madlib'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Madlib />
+    </div>
+  );
+}
+
+export default App;
