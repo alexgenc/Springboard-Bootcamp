b06dd1927924a2bf102481f80df284c03c5fb59a
AHG <ahakangenc92@gmail.com>
2021-01-28 20:15:41 +0300

    VendingMachine component

--- 41.1_React_Router_Intro/src/VendingMachine.js
+++ 41.1_React_Router_Intro/src/VendingMachine.js
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import Chips from './Chips';
+import Soda from './Soda';
+import Sardines from './Sardines';
+import './VendingMachine.css';
 
 const VendingMachine = () => {
   return (
-    <div>
+    <div className="VendingMachine">
       <h1>Vending Machine</h1>
+      
+      <BrowserRouter>
+        <NavLink exact to ="/chips">Chips</NavLink>
+        <NavLink exact to ="/soda">Soda</NavLink>
+        <NavLink exact to ="/sardines">Sardines</NavLink>
+        
+        {/* Routes */}
+        <Route exact path ="/chips">
+          <Chips />
+        </Route>
+        <Route exact path ="/soda">
+          <Soda />
+        </Route>
+        <Route exact path ="/Sardines">
+          <Sardines />
+        </Route>
+  
+      </BrowserRouter>
     </div>
   )
 }
