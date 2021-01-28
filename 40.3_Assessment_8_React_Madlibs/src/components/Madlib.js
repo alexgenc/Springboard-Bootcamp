b4b9b8efb0d5fbacd47cdd3bd3c6d45f8ae9abd3
AHG <ahakangenc92@gmail.com>
2021-01-28 15:47:07 +0300

    Component for displaying Madlib form as well as story results

--- 40.3_Assessment_8_React_Madlibs/src/components/Madlib.js
+++ 40.3_Assessment_8_React_Madlibs/src/components/Madlib.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import MadlibForm from './MadlibForm';
 
 const Madlib = () => {
+
+  const [story, setStory] = useState('');
+  const [showResult, setShowResult] = useState(false);
+
+  const createStory = (story) => {
+    const {selection, noun1, noun2, adjective, color} = story;
+    
+    if (selection === "Happy") {
+      setStory(`There was a ${color} ${noun1} who loved a ${adjective} ${noun2}`);
+    } else {
+      setStory(`There was a ${color} ${noun1} who hated a ${adjective} ${noun2}`);
+    }
+
+    setShowResult(true);
+  }
+
+  const resetForm = () => {
+    setShowResult(false);
+  }
+
   return (
     <div>
-      <h1>Hi</h1>
+      <h1>Madlibs!</h1>
+      {!showResult &&
+        <MadlibForm createStory={createStory} />
+      }
+      {showResult && 
+        <div>
+          <p>
+            {story}
+          </p> 
+          <button onClick={resetForm}>Restart</button>
+        </div>
+      }
     </div>
   )
 }
