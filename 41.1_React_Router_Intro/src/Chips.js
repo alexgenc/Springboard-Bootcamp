9d02d6e618071ddc1845ea84134ddae084df020c
AHG <ahakangenc92@gmail.com>
2021-01-28 20:14:54 +0300

    Chips component

---
+++ 41.1_React_Router_Intro/src/Chips.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chips = () => {
+  return (
+    <div>
+      <img src="https://image.made-in-china.com/202f0j00ufhUmWyGhgkZ/Automatic-Vertical-Small-Potato-Chips-Bag-Packing-Machine.jpg"></img>
+    </div>
+  )
+}
+
+export default Chips;
