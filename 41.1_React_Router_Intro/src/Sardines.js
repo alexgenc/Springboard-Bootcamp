9e2484c69e5448b61cbe77e759253e1a34d41aa0
AHG <ahakangenc92@gmail.com>
2021-01-28 20:15:15 +0300

    Sardines component

---
+++ 41.1_React_Router_Intro/src/Sardines.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sardines = () => {
+  return (
+    <div>
+      <img src="https://i2.wp.com/unboundwellness.com/wp-content/uploads/2019/04/sardines2.jpg"></img>
+    </div>
+  )
+}
+
+export default Sardines;
