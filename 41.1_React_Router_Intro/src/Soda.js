16b74d42119968e0c79ee99315038e80e10530b3
AHG <ahakangenc92@gmail.com>
2021-01-28 20:15:06 +0300

    Soda component

---
+++ 41.1_React_Router_Intro/src/Soda.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Soda = () => {
+  return (
+    <div>
+      <img src="https://www.tbotech.com/images/thumbnails/465/465/detailed/2/coke-can-secret-safe.png"></img>
+    </div>
+  )
+}
+
+export default Soda;
