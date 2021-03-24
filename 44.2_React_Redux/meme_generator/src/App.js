6cd6e591629fb2f2c2c558dbdea93e0e8f512747
AHG <ahakangenc92@gmail.com>
2021-03-24 16:58:17 -0500

    App component

--- 44.2_React_Redux/meme_generator/src/App.js
+++ 44.2_React_Redux/meme_generator/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import Meme from './Meme';
+import NewMemeForm from './NewMemeForm';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
+
+  const memes = useSelector(st => st.memes);
+  const dispatch = useDispatch();
+
+  function addMeme(newMeme) {
+    dispatch({ type: "ADD_MEME", meme: newMeme });
+  }
+
+  function deleteMeme(id) {
+    dispatch({type: "REMOVE_MEME", id });
+  }
+
+  const memeContainer = memes.map(m => (
+    <Meme
+      key={m.id}
+      topText={m.topText}
+      bottomText={m.bottomText}
+      imageURL={m.imageURL}
+      deleteMeme={() => deleteMeme(m.id)}
+    />
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewMemeForm addMeme={addMeme} />
+      <hr />
+      {memeContainer}
     </div>
   );
 }
