aa76c25f517b1eb10117fe65836c5e569b8a3701
AHG <ahakangenc92@gmail.com>
2021-03-23 21:21:32 -0500

    Update routes, create a function for adding new drinks and snacks

--- 42.3_Assessment_9_Snack_or_Booze/src/App.js
+++ 42.3_Assessment_9_Snack_or_Booze/src/App.js
@@ -6,16 +6,20 @@ import SnackOrBoozeApi from "./Api";
 import NavBar from "./NavBar";
 import { Route, Switch } from "react-router-dom";
 import Menu from "./FoodMenu";
-import Snack from "./FoodItem";
+import Item from "./FoodItem";
+import NewItemForm from "./NewItemForm";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [snacks, setSnacks] = useState([]);
+  const [drinks, setDrinks] = useState([]);
 
   useEffect(() => {
     async function getSnacks() {
       let snacks = await SnackOrBoozeApi.getSnacks();
+      let drinks = await SnackOrBoozeApi.getDrinks();
       setSnacks(snacks);
+      setDrinks(drinks);
       setIsLoading(false);
     }
     getSnacks();
@@ -25,6 +29,16 @@ function App() {
     return <p>Loading &hellip;</p>;
   }
 
+  const addItem = async (item) => {
+    if (item.type === "snack") {
+      let newSnack = await SnackOrBoozeApi.addNewSnack(item);
+      setSnacks( (snacks) => [...snacks, newSnack] );
+    } else {
+      let newDrink = await SnackOrBoozeApi.addNewDrink(item);
+      setDrinks( (drinks) => [...drinks, newDrink] );
+    }
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -32,13 +46,22 @@ function App() {
         <main>
           <Switch>
             <Route exact path="/">
-              <Home snacks={snacks} />
+              <Home snacks={snacks} drinks={drinks} />
             </Route>
             <Route exact path="/snacks">
               <Menu snacks={snacks} title="Snacks" />
             </Route>
+            <Route exact path="/drinks">
+              <Menu drinks={drinks} title="Drinks" />
+            </Route>
             <Route path="/snacks/:id">
-              <Snack items={snacks} cantFind="/snacks" />
+              <Item items={snacks} cantFind="/snacks" />
+            </Route>
+            <Route path="/drinks/:id">
+              <Item items={drinks} cantFind="/drinks" />
+            </Route>
+            <Route path="/new">
+              <NewItemForm addItem={addItem} />
             </Route>
             <Route>
               <p>Hmmm. I can't seem to find what you want.</p>
