45d3dab701edf6c063503d66451aebfada189841
AHG <ahakangenc92@gmail.com>
2021-03-26 14:57:52 -0500

    Navbar component

---
+++ 44.3_React_Redux_Patterns/src/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { calculateTotalQuantity } from "./helpers";
+
+function Navbar() {
+  // Get # of items and cart value from redux store
+  const itemCount = useSelector(store => calculateTotalQuantity(store.cartItems));
+  const cartValue = useSelector(store => store.cartValue);
+  // Account for multiple items
+  const itemUnit = itemCount === 1 ? "item" : "items";
+  
+  return (
+    <nav className="navbar navbar-light bg-info">
+      <Link to="/" className="navbar-brand text-light">
+        SHOPLY!
+      </Link>
+      <ul className="navbar-nav flex-row">
+        <li className="nav-item pr-3">
+          <span className="navbar-text text-light">
+            {itemCount} {itemUnit} (${cartValue})
+          </span>
+        </li>
+        <li className="nav-item">
+          <Link to="/cart" className="nav-link text-light">
+            See Cart
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
