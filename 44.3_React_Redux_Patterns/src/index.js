7e6fb7c54c32b7a9125ae2361336fd86c72b5979
AHG <ahakangenc92@gmail.com>
2021-03-26 15:01:19 -0500

    Wrap app with redux provider

--- 44.3_React_Redux_Patterns/src/index.js
+++ 44.3_React_Redux_Patterns/src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import rootReducer from './rootReducer';
+import { createStore } from 'redux';
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
